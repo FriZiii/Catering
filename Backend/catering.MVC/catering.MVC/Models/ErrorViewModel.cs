77f46217a2381c0b5779ccdf8d62b3afa1721a4c
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-05-31 15:02:09 +0200

    New project created

---
+++ Backend/catering.MVC/catering.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace catering.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
