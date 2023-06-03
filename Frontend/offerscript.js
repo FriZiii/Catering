11b1b83857d93612615b49f3334a1502f84f55ba
MadziaSzmatloch <98941475+MadziaSzmatloch@users.noreply.github.com>
2023-06-03 15:54:24 +0200

    modal improved

--- Frontend/offerscript.js
+++ Frontend/offerscript.js
@@ -1,6 +1,6 @@
 const modal_container = document.getElementById('offer-modal-container');
 const openModal = document.getElementById('open-modal-button');
-const closeModal = document.getElementById('add-to-cart-button');
+const closeModal = document.getElementById('close-modal-button');
 
 openModal.addEventListener('click', () => {
     modal_container.classList.add('show');
