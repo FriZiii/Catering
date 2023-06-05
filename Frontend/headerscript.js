3a8eb9532316c3274db7a0d9717812df37e566d9
714lipca <jakubpietrasik16@gmail.com>
2023-06-05 15:50:03 +0200

    feat: repaired burger-menu, changed offer

--- Frontend/headerscript.js
+++ Frontend/headerscript.js
@@ -1,9 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
-const hamburger_icon = hamburger.querySelector('span');
+const hamburger_icon = hamburger.querySelector('.material-icons');
 const mobile_menu = document.querySelector('.mobile-menu');
 
 hamburger.addEventListener('click', () => {
 	hamburger_icon.innerText = hamburger_icon.innerText === 'menu' ? 'close': 'menu';
-	
+	console.log("elo")
 	mobile_menu.classList.toggle('is-open');
 })
