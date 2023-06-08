d9aeeb8116cd435c19c8e23bd345118554a769d3
714lipca <jakubpietrasik16@gmail.com>
2023-06-08 10:45:54 +0200

    feat: offer changed

--- Frontend/headerscript.js
+++ Frontend/headerscript.js
@@ -1,9 +1,9 @@
-const hamburger = document.querySelector('.hamburger');
-const hamburger_icon = hamburger.querySelector('.material-icons');
-const mobile_menu = document.querySelector('.mobile-menu');
+// Get the hamburger button and mobile menu elements
+    const hamburgerButton = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-menu');
 
-hamburger.addEventListener('click', () => {
-	hamburger_icon.innerText = hamburger_icon.innerText === 'menu' ? 'close': 'menu';
-	console.log("elo")
-	mobile_menu.classList.toggle('is-open');
-})
+    // Add click event listener to the hamburger button
+    hamburgerButton.addEventListener('click', function() {
+        // Toggle the 'active' class on the mobile menu
+        mobileMenu.classList.toggle('is-open');
+    });
