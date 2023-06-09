0c1c85de64db80556b7eea9ec824afa0de1a60c6
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-06-10 00:04:09 +0200

    folder restructuring, changing namespaces

--- Backend/catering.MVC/catering.Application/Mappings/MappingsProfile.cs
+++ Backend/catering.MVC/catering.Application/Mappings/MappingsProfile.cs
@@ -1,6 +1,6 @@
 ﻿using AutoMapper;
-using catering.Application.DTO.Cart;
-using catering.Application.DTO.Offer;
+using catering.Application.Managements.CartManagement.Cart;
+using catering.Application.Managements.OfferManagment;
 using catering.Domain.Entities;
 using catering.Domain.Entities.CartEntities;
 using catering.Domain.Interface;
