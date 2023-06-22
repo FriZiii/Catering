d3ffedc96e3d244598823786ab46dfb0f0734d83
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-06-22 16:21:00 +0200

    Added register controler and dto

---
+++ Backend/catering.MVC/catering.Application/Managements/AccountManagment/AccountDtos/RegisterInputDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catering.Application.Managements.AccountManagment.AccountDtos
+{
+    public class RegisterInputDto
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public string PasswordConfirm { get; set; } = default!;
+        public DateTime BirthDate { get; set; } = default!;
+
+        public string Adress1 { get; set; } = default!;
+        public string Adress2 { get; set; } = default!;
+        public string PostalCode { get; set; } = default!;
+        public string State { get; set; } = default!;
+        public string Country { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+    }
+}
---
+++ Backend/catering.MVC/catering.MVC/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using catering.Application.Managements.AccountManagment.AccountDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace catering.MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterInputDto registerInputDto)
+        {
+            return View();
+        }
+    }
+}
