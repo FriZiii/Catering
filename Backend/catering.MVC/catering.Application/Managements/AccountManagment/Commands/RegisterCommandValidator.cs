c328312fb88f0a2631d0bedcdf4ca00ef7beb36a
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-06-23 02:18:47 +0200

    Added validation for register

---
+++ Backend/catering.MVC/catering.Application/Managements/AccountManagment/Commands/RegisterCommandValidator.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catering.Application.Managements.AccountManagment.Commands
+{
+    public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterCommandValidator()
+        {
+            RuleFor(c => c.FirstName)
+                .NotEmpty().WithMessage("First name is required.");
+
+            RuleFor(c => c.LastName)
+                .NotEmpty().WithMessage("Last name is required.");
+
+            RuleFor(c => c.Password)
+                .NotEmpty().WithMessage("Password is required.");
+
+            RuleFor(c => c.PasswordConfirm)
+                .Equal(c => c.Password).WithMessage("Passwords do not match.");
+
+            RuleFor(c => c.Email)
+                .NotEmpty().WithMessage("Email is required.");
+
+            RuleFor(c => c.BirthDate)
+                .NotEmpty().WithMessage("Birth date is required.");
+
+            RuleFor(c => c.Adress1)
+                .NotEmpty().WithMessage("Address 1 is required.");
+
+            RuleFor(c => c.Adress2)
+                .NotEmpty().WithMessage("Address 2 is required.");
+
+            RuleFor(c => c.PostalCode)
+                .NotEmpty().WithMessage("Postal code is required.");
+
+            RuleFor(c => c.State)
+                .NotEmpty().WithMessage("State/Region is required.");
+
+            RuleFor(c => c.Country)
+                .NotEmpty().WithMessage("Country is required.");
+
+            RuleFor(c => c.PhoneNumber)
+                .NotEmpty().WithMessage("Phone number is required.");
+        }
+    }
+}
--- Backend/catering.MVC/catering.MVC/Controllers/AccountController.cs
+++ Backend/catering.MVC/catering.MVC/Controllers/AccountController.cs
@@ -1,18 +1,33 @@
 ﻿using catering.Application.Managements.AccountManagment.AccountDtos;
+using catering.Application.Managements.AccountManagment.Commands;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace catering.MVC.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IMediator mediator;
+
+        public AccountController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
         public IActionResult Register()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Register(RegisterInputDto registerInputDto)
+        public async Task<IActionResult> Register(RegisterCommand registerCommand)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(registerCommand);
+            }
+            await mediator.Send(registerCommand);
             return View();
         }
     }
