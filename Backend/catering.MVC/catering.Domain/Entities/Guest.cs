16a3002ab97c4a26812b81466099ca58bf70223e
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-06-26 01:19:47 +0200

    Addet Guest entity

---
+++ Backend/catering.MVC/catering.Domain/Entities/Guest.cs
@@ -0,0 +1,12 @@
+﻿using catering.Domain.Entities.User.AppUser;
+
+namespace catering.Domain.Entities
+{
+    public class Guest
+    {
+        public string Id { get; set; } = null!;
+        public int DeliveryAdressId { get; set; }
+
+        public UserDeliveryAdress DeliveryAdress { get; set; } = null!;
+    }
+}
---
+++ Backend/catering.MVC/catering.Infrastructure/Migrations/20230625224619_AddedGuest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace catering.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedGuest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Guests",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DeliveryAdressId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Guests_UserDeliveryAdress_DeliveryAdressId",
+                        column: x => x.DeliveryAdressId,
+                        principalTable: "UserDeliveryAdress",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guests_DeliveryAdressId",
+                table: "Guests",
+                column: "DeliveryAdressId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Guests");
+        }
+    }
+}
