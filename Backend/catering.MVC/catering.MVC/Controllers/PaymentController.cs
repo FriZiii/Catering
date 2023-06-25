119c139b241e04f2fad4532a6f720c99020f34b1
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-06-25 15:11:53 +0200

    Added payment controller and view

---
+++ Backend/catering.MVC/catering.MVC/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace catering.MVC.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
