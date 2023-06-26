1e49a3c79f96842525344c30b6dc75d1dc735425
714lipca <jakubpietrasik16@gmail.com>
2023-06-26 12:59:49 +0200

    admin buttons work

---
+++ Frontend/divsadminscript.js
@@ -0,0 +1,11 @@
+var currentDiv = null;
+
+function showDiv(divNumber) {
+    if (currentDiv !== null) {
+        currentDiv.style.display = "none";
+    }
+
+    var div = document.getElementById("div" + divNumber + "-admin");
+    div.style.display = "flex";
+    currentDiv = div;
+}
