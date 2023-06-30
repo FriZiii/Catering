538a16d2fb5d285589b93d91aff34288d0220be9
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-06-30 14:02:50 +0200

    Now the user also receives their orders

--- Backend/catering.MVC/catering.Application/Managements/AccountManagment/AccountDtos/CurrentUser.cs
+++ Backend/catering.MVC/catering.Application/Managements/AccountManagment/AccountDtos/CurrentUser.cs
@@ -6,7 +6,7 @@ namespace catering.Application.Managements.AccountManagment.AccountDtos
     {
         public string Id { get; set; } = default!;
         public string Email { get; set; } = default!;
-        public ICollection<Order> Orders { get; set; } = default!;
+        public IEnumerable<Order> Orders { get; set; } = default!;
         public IEnumerable<string> Roles { get; set; } = default!;
         public bool IsInRole(string role)
         {
--- Backend/catering.MVC/catering.Application/Managements/AccountManagment/Querries/GetCurrentUser/GetCurrentUserQuerryHandler.cs
+++ Backend/catering.MVC/catering.Application/Managements/AccountManagment/Querries/GetCurrentUser/GetCurrentUserQuerryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using catering.Application.Managements.AccountManagment.AccountDtos;
+using catering.Application.Managements.OrderManagment.Queries.GetOrderByUserId;
 using catering.Domain.Interface;
 using MediatR;
 using System.Security.Claims;
@@ -10,11 +11,13 @@ namespace catering.Application.Managements.AccountManagment.Querries.GetCurrentU
     {
         private readonly IMapper mapper;
         private readonly IAccountRepository accountRepository;
+        private readonly IMediator mediator;
 
-        public GetCurrentUserQuerryHandler(IMapper mapper, IAccountRepository accountRepository)
+        public GetCurrentUserQuerryHandler(IMapper mapper, IAccountRepository accountRepository, IMediator mediator)
         {
             this.mapper = mapper;
             this.accountRepository = accountRepository;
+            this.mediator = mediator;
         }
 
         public async Task<CurrentUser?> Handle(GetCurrentUserQuerry request, CancellationToken cancellationToken)
@@ -23,6 +26,7 @@ namespace catering.Application.Managements.AccountManagment.Querries.GetCurrentU
             if(user is not null)
             {
                 var currentUser = mapper.Map<CurrentUser>(user);
+                currentUser.Orders = await mediator.Send(new GetOrdersByUserIdQuerry(currentUser.Id));
                 return currentUser;
             }
             return null;
