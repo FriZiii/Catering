900fccf953dc846da9ceb52b9e13859f302b225b
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-02 15:55:35 +0200

    Added filtering of results

--- Backend/catering.MVC/catering.MVC/wwwroot/js/admin-dashboard.js
+++ Backend/catering.MVC/catering.MVC/wwwroot/js/admin-dashboard.js
@@ -19,3 +19,28 @@ function showDiv(divNumber) {
     div.style.display = "flex";
     currentDiv = div;
 }
+
+const searchProductInput = document.getElementById('search-product');
+const rows = document.querySelectorAll('tbody tr');
+const counter = document.getElementsByClassName('search-counter-admin')[0];
+
+searchProductInput.addEventListener('keyup', function (event) {
+    const q = event.target.value.toLowerCase();
+    let count = 0;
+    rows.forEach((row) => {
+        let matchFound = false;
+        const records = row.querySelectorAll('td');
+        records.forEach(record => {
+            if (record.textContent.toLowerCase().includes(q)) {
+                matchFound = true;
+            }
+        });
+        if (matchFound === true) {
+            row.style.display = '';
+            count += 1;
+        } else {
+            row.style.display = 'none';
+        }
+    });
+    counter.textContent = count;
+});
