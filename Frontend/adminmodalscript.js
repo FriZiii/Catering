24bca3e9738449f7388bdbce3f03aaf1d29da9e0
714lipca <jakubpietrasik16@gmail.com>
2023-07-02 22:41:46 +0200

    upload file style changed

--- Frontend/adminmodalscript.js
+++ Frontend/adminmodalscript.js
@@ -8,3 +8,12 @@ function closeModalAdminProduct() {
     document.getElementById("modal-create-admin").style.display = "none";
     document.getElementById("header-admin").style.zIndex = 1;
 }
+
+const file = document.querySelector('#file');
+file.addEventListener('change', (e) => {
+  const [file] = e.target.files;
+  const { name: fileName, size } = file;
+  const fileSize = (size / 1000).toFixed(2);
+  const fileNameAndSize = `${fileName} - ${fileSize}KB`;
+  document.querySelector('.file-name').textContent = fileNameAndSize;
+});
