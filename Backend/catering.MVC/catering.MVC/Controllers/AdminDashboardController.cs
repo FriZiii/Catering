7e64d78a3adb0821de16df7059825d800c831368
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-02 15:54:33 +0200

    Added view and view model for dashboard

--- Backend/catering.MVC/catering.MVC/Controllers/AdminDashboardController.cs
+++ Backend/catering.MVC/catering.MVC/Controllers/AdminDashboardController.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using catering.Application.Managements.OfferManagment.Queries.GetAllProducts;
+using catering.MVC.Models;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace catering.MVC.Controllers
 {
     public class AdminDashboardController : Controller
     {
-        [Authorize(Roles = "Admin")]
-        public IActionResult Index()
+        private readonly IMediator mediator;
+
+        public AdminDashboardController(IMediator mediator)
         {
-            return View();
+            this.mediator = mediator;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Index()
+        {
+            var products = await mediator.Send(new GetAllProductsQuery());
+
+            var dashboardViewModel = new AdminDashboardViewModel()
+            {
+                Products = products,
+            };
+
+            return View(dashboardViewModel);
         }
     }
 }
---
+++ Backend/catering.MVC/catering.MVC/Models/AdminDashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using catering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catering.MVC.Models
+{
+    public class AdminDashboardViewModel
+    {
+        public IEnumerable<Product> Products { get; set; } = null!;
+    }
+}
