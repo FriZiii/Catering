b859030361f69bbecee03383258b56a61d1f230b
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-02 15:55:16 +0200

    Added js for dashboard

---
+++ Backend/catering.MVC/catering.MVC/wwwroot/js/admin-dashboard.js
@@ -0,0 +1,21 @@
+﻿function openModalAdminProduct() {
+    document.getElementById("modal-create-admin").style.display = "block";
+    document.getElementById("header-admin").style.zIndex = -1;
+}
+
+function closeModalAdminProduct() {
+    document.getElementById("modal-create-admin").style.display = "none";
+    document.getElementById("header-admin").style.zIndex = 1;
+}
+
+var currentDiv = null;
+
+function showDiv(divNumber) {
+    if (currentDiv !== null) {
+        currentDiv.style.display = "none";
+    }
+
+    var div = document.getElementById("div" + divNumber + "-admin");
+    div.style.display = "flex";
+    currentDiv = div;
+}
