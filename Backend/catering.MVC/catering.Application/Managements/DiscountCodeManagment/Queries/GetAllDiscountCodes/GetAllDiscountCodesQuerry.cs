35a308c8ba216b8549cc025d3caf5cc9b6424021
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-03 13:40:39 +0200

    Added querry for getting all discounts

---
+++ Backend/catering.MVC/catering.Application/Managements/DiscountCodeManagment/Queries/GetAllDiscountCodes/GetAllDiscountCodesQuerry.cs
@@ -0,0 +1,15 @@
+﻿using catering.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catering.Application.Managements.DiscountCodeManagment.Queries.GetAllDiscountCodes
+{
+    public class GetAllDiscountCodesQuerry : IRequest<IEnumerable<DiscountCode>>
+    {
+
+    }
+}
