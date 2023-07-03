9a4c2223902aea1278d010d3edf11eca239538c5
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-03 11:42:24 +0200

    custom button for uplouding files

--- Backend/catering.MVC/catering.MVC/wwwroot/js/admin-dashboard.js
+++ Backend/catering.MVC/catering.MVC/wwwroot/js/admin-dashboard.js
@@ -1,4 +1,5 @@
-﻿function openModalAdminProduct() {
+﻿//Modal
+function openModalAdminProduct() {
     document.getElementById("modal-create-admin").style.display = "block";
     document.getElementById("header-admin").style.zIndex = -1;
 }
@@ -8,6 +9,17 @@ function closeModalAdminProduct() {
     document.getElementById("header-admin").style.zIndex = 1;
 }
 
+const file = document.querySelector('#file');
+file.addEventListener('change', (e) => {
+    const [file] = e.target.files;
+    const { name: fileName, size } = file;
+    const fileSize = (size / 1000).toFixed(2);
+    const fileNameAndSize = `${fileName} - ${fileSize}KB`;
+    document.querySelector('.file-name').textContent = fileNameAndSize;
+});
+
+
+//Switchings divs
 var currentDiv = null;
 
 function showDiv(divNumber) {
@@ -20,6 +32,16 @@ function showDiv(divNumber) {
     currentDiv = div;
 }
 
+document.addEventListener("DOMContentLoaded", function (event) {
+    showDiv(1);
+});
+
+function changeIcon(button, imagePath) {
+    var img = button.querySelector('.icon img');
+    img.src = imagePath;
+}
+
+//Filtering
 const searchProductInput = document.getElementById('search-product');
 const rows = document.querySelectorAll('tbody tr');
 const counter = document.getElementsByClassName('search-counter-admin')[0];
