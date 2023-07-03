cf826f06a0b0e2b54d9d3b8cac93624bc8e746b4
714lipca <jakubpietrasik16@gmail.com>
2023-07-03 18:24:21 +0200

    user-panel added

---
+++ Frontend/userpanelscript.js
@@ -0,0 +1,20 @@
+var currentDiv = null;
+
+function showDiv(divNumber) {
+    if (currentDiv !== null) {
+        currentDiv.style.display = "none";
+    }
+
+    var div = document.getElementById("div" + divNumber + "-admin");
+    div.style.display = "flex";
+    currentDiv = div;
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    showDiv(1);
+});
+
+function changeIcon(button, imagePath) {
+    var img = button.querySelector('.icon img');
+    img.src = imagePath;
+  }
