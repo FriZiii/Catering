354c64abf941a872f75a2dce55d1c19cb45ae9a8
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-05 01:16:09 +0200

    Added displaing order items in the dashboard

--- Backend/catering.MVC/catering.MVC/Controllers/AdminDashboardController.cs
+++ Backend/catering.MVC/catering.MVC/Controllers/AdminDashboardController.cs
@@ -1,7 +1,4 @@
-﻿using catering.Application.Managements.AccountManagment.Querries.GetAllUsers;
-using catering.Application.Managements.DiscountCodeManagment.Queries.GetAllDiscountCodes;
-using catering.Application.Managements.OfferManagment.Queries.GetAllProducts;
-using catering.Application.Managements.OrderManagment.Queries.GetAllOrders;
+﻿using catering.Application.Managements.OrderManagment.Queries.GetAllOrders;
 using catering.MVC.Models;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -21,17 +18,11 @@ namespace catering.MVC.Controllers
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Index()
         {
-            var products = await mediator.Send(new GetAllProductsQuery());
             var orders = await mediator.Send(new GetAllOrdersQuerry());
-            var discounts = await mediator.Send(new GetAllDiscountCodesQuerry());
-            var users = await mediator.Send(new GetAllUsersQuerry());
 
             var dashboardViewModel = new AdminDashboardViewModel()
             {
-                Products = products,
                 Orders = orders,
-                Discounts = discounts,
-                Users = users
             };
 
             return View(dashboardViewModel);
--- Backend/catering.MVC/catering.MVC/Models/AdminDashboardViewModel.cs
+++ Backend/catering.MVC/catering.MVC/Models/AdminDashboardViewModel.cs
@@ -1,23 +1,12 @@
 ﻿using catering.Application.Managements.DiscountCodeManagment.Commands.CreateDiscountCode;
-using catering.Application.Managements.OfferManagment;
 using catering.Application.Managements.OfferManagment.Commands.AddProduct;
-using catering.Domain.Entities;
 using catering.Domain.Entities.OrderEntities;
-using catering.Domain.Entities.User.AppUser;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace catering.MVC.Models
 {
     public class AdminDashboardViewModel
     {
-        public IEnumerable<Product> Products { get; set; } = null!;
         public IEnumerable<Order> Orders { get; set; } = null!;
-        public IEnumerable<DiscountCode> Discounts { get; set; } = null!;
-        public IEnumerable<AppUser> Users { get; set; } = null!;
 
         public CreateProductCommand CreateProductCommand { get; set; } = null!;
         public CreateDiscountCodeCommand CreateDiscountCodeCommand { get; set; } = null!;
