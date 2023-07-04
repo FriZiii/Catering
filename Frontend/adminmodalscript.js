2162c6d007c2d5340eedd302076e35c8b40d5731
Jakub Pietrasik <jakubmkjp@gmail.com>
2023-07-04 16:25:58 +0200

    modal admin changed

--- Frontend/adminmodalscript.js
+++ Frontend/adminmodalscript.js
@@ -2,6 +2,7 @@
 function openModalAdminProduct() {
     document.getElementById("modal-create-admin").style.display = "block";
     document.getElementById("header-admin").style.zIndex = -1;
+    document.querySelector('.file-name').textContent = "";
 }
 
 function closeModalAdminProduct() {
