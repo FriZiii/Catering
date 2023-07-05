e3b713907c95e657329ce141f3c1e693149000f6
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-06 00:51:06 +0200

    Added controller and view for user dashboard

---
+++ Backend/catering.MVC/catering.MVC/Controllers/UserDashboardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace catering.MVC.Controllers
+{
+    public class UserDashboardController : Controller
+    {
+        public UserDashboardController()
+        {
+            
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
