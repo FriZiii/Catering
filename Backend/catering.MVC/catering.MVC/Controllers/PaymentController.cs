fc013ff8d847126d371eeca547b04834ab608c16
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-06 21:46:34 +0200

    Added payment logic

--- Backend/catering.MVC/catering.MVC/Controllers/PaymentController.cs
+++ Backend/catering.MVC/catering.MVC/Controllers/PaymentController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using catering.Application.Managements.OrderManagment.Commands.DeleteOrderById;
+using catering.Application.Managements.OrderManagment.Commands.MarkAsPaid;
+using catering.Application.Managements.OrderManagment.Queries.GetOrderIdFromCookies;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace catering.MVC.Controllers
 {
     public class PaymentController : Controller
     {
+        private readonly IMediator mediator;
+
+        public PaymentController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Paid()
+        {
+            int orderId = await mediator.Send(new GetOrderIdFromCookiesQuery());
+            await mediator.Send(new MarkAsPaidCommand(orderId));
+            return Ok();
         }
     }
 }
---
+++ Backend/catering.MVC/catering.MVC/wwwroot/js/payment.js
@@ -0,0 +1,12 @@
+﻿const Paid = () => {
+    $.ajax({
+        url: '/Payment/Paid',
+        type: 'Post',
+        success: function () {
+            window.location.href = '/';
+        }
+    })
+}
+
+const button = document.getElementsByClassName('payment-btn-with-icon')[0];
+button.addEventListener('click', Paid);
