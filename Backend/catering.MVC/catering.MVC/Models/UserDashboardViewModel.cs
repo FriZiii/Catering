8b560f0d86274fd564ad81ef70e73f93614452fc
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-07 15:10:58 +0200

    Added displaying orders in user dashboard

---
+++ Backend/catering.MVC/catering.MVC/Models/UserDashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using catering.Domain.Entities.OrderEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catering.MVC.Models
+{
+    public class UserDashboardViewModel
+    {
+        public IEnumerable<Order> Orders { get; set; } = default!;
+
+    }   
+}
