a95848c525ba36736c838449238235a3dda8c567
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-07 15:47:36 +0200

    Added better validations and improved messages

--- Backend/catering.MVC/catering.Application/Managements/AccountManagment/Commands/Register/RegisterCommandValidator.cs
+++ Backend/catering.MVC/catering.Application/Managements/AccountManagment/Commands/Register/RegisterCommandValidator.cs
@@ -12,40 +12,43 @@ namespace catering.Application.Managements.AccountManagment.Commands.Register
         public RegisterCommandValidator()
         {
             RuleFor(c => c.FirstName)
-                .NotEmpty().WithMessage("First name is required.");
+                .NotEmpty().WithMessage("Please provide your first name.");
 
             RuleFor(c => c.LastName)
-                .NotEmpty().WithMessage("Last name is required.");
+                 .NotEmpty().WithMessage("Please provide your last name.");
 
             RuleFor(c => c.Password)
-                .NotEmpty().WithMessage("Password is required.");
+                .NotEmpty().WithMessage("Please provide a password.");
 
             RuleFor(c => c.PasswordConfirm)
-                .Equal(c => c.Password).WithMessage("Passwords do not match.");
+                .Equal(c => c.Password).WithMessage("Passwords do not match. Please ensure both passwords are the same.");
 
             RuleFor(c => c.Email)
-                .NotEmpty().WithMessage("Email is required.");
+                .NotEmpty().WithMessage("Please provide your email address.")
+                .EmailAddress().WithMessage("Please enter a valid email address.");
 
             RuleFor(c => c.BirthDate)
-                .NotEmpty().WithMessage("Birth date is required.");
+                .NotEmpty().WithMessage("Please provide your birth date.");
 
             RuleFor(c => c.Adress1)
-                .NotEmpty().WithMessage("Address 1 is required.");
+                .NotEmpty().WithMessage("Please provide your address (line 1).");
 
             RuleFor(c => c.Adress2)
-                .NotEmpty().WithMessage("Address 2 is required.");
+                .NotEmpty().WithMessage("Please provide your address (line 2).");
 
             RuleFor(c => c.PostalCode)
-                .NotEmpty().WithMessage("Postal code is required.");
+                .NotEmpty().WithMessage("Please provide your postal code.")
+                .Matches(@"^\d{2}-\d{3}$").WithMessage("Please enter a valid postal code in the format XX-XXX.");
 
             RuleFor(c => c.State)
-                .NotEmpty().WithMessage("State/Region is required.");
+                .NotEmpty().WithMessage("Please provide your state or region.");
 
             RuleFor(c => c.Country)
-                .NotEmpty().WithMessage("Country is required.");
+                .NotEmpty().WithMessage("Please provide your country.");
 
             RuleFor(c => c.PhoneNumber)
-                .NotEmpty().WithMessage("Phone number is required.");
+                .NotEmpty().WithMessage("Please provide your phone number.")
+                .Matches(@"^[0-9]{9,}$").WithMessage("Please enter a valid phone number containing at least 9 numeric digits.");
         }
     }
 }
