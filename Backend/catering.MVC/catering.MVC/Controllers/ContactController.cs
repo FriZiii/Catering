74ded6dff99ca8026e14af3b2b81e1357bf8cb95
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-10 13:34:27 +0200

    Added contact view and controller

---
+++ Backend/catering.MVC/catering.MVC/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace catering.MVC.Controllers
+{
+    public class ContactController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
