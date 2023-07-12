bb262fc539f22f9c187b3bd18643ca9a95f6d5b7
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-12 14:50:56 +0200

    Added form input retrieval

--- Backend/catering.MVC/catering.MVC/Controllers/ContactController.cs
+++ Backend/catering.MVC/catering.MVC/Controllers/ContactController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using catering.Application.Managements.MessageManagement.MessageDto;
+using Microsoft.AspNetCore.Mvc;
 
 namespace catering.MVC.Controllers
 {
@@ -7,6 +8,11 @@ namespace catering.MVC.Controllers
         public async Task<IActionResult> Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Submit(MessageDto messageDto)
+        {
+            return RedirectToAction("Index");
         }
     }
 }
