fc77abbe66cff7504475d3293a99d5574cdaa618
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-07-12 14:49:46 +0200

    Added message dto

---
+++ Backend/catering.MVC/catering.Application/Managements/MessageManagement/MessageDto/MessageDto.cs
@@ -0,0 +1,10 @@
+﻿namespace catering.Application.Managements.MessageManagement.MessageDto
+{
+    public class MessageDto
+    {
+        public string FirstLastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Subject { get; set; } = null!;
+        public string Message { get; set; } = null!;
+    }
+}
